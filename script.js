5b2feb0f615111c6d0abf9dfcd8a59b24d5c7bc5
Kranthi Aravelli <aravellykranthi@gmail.com>
2026-06-17 07:19:57 +0530

    Enhance navigation toggle and scrolling behavior
    Refactor navigation functionality with improved event handling and section highlighting.

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-const menuToggle = document.getElementById("menu-toggle");
-const navElements = document.getElementById("nav-elements");
+const navLinks = document.querySelectorAll('nav a');
+const sections = document.querySelectorAll('section');
+const menuToggle = document.querySelector('.menu-toggle');
+const navId = document.getElementById('nav-elements');
 
-menuToggle.addEventListener("click", () => {
-    navElements.classList.toggle("active");
+menuToggle.addEventListener('click', function() {
+    navId.style.display = 'block'
 })
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        navLinks.forEach(item => item.classList.remove('selected'));
+
+        this.classList.add('selected');
+    });
+});
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+
+        if (window.scrollY >= sectionTop - 300){
+            current = section.id;
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('selected');
+
+        if(link.getAttribute('href') === `#${current}`){
+            link.classList.add('selected')
+        }
+    });
+});
